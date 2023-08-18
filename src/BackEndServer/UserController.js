dac72d4a1b8e0a28c4ebe898c696da6993b254f1
HuyLemm <121721803+HuyLemm@users.noreply.github.com>
2023-08-18 17:16:16 +0700

    Update UserController.js

--- src/BackEndServer/UserController.js
+++ src/BackEndServer/UserController.js
@@ -1,25 +1,47 @@
-class UserController {
-    postSignUp(userData) {
-      // Xử lý logic đăng ký tài khoản người dùng
-    }
-  
-    postLogin(username, password) {
-      // Xử lý logic đăng nhập
-    }
-  
-    postCheckOrderInfo(orderId) {
-      // Xử lý logic kiểm tra thông tin đơn hàng
-    }
-  
-    postConfirmOrdering(orderId) {
-      // Xử lý logic xác nhận đặt hàng
-    }
-  
-    postLogout() {
-      // Xử lý logic đăng xuất
-    }
-  
-    postPurchase(orderData) {
-      // Xử lý logic thanh toán và hoàn tất đơn hàng
-    }
+const axios = require('axios');
+
+const serverURL = 'http://localhost:9999'; // Thay đổi URL của server tại đây
+
+async function signUp(user) {
+  try {
+    const signUpURL = `${serverURL}/users/signUp`;
+
+    const response = await axios.post(signUpURL, user);
+    console.log('Data sent:', response.data);
+  } catch (error) {
+    console.error('Error occurred during sign up:', error.response.data);
   }
+}
+
+async function signIn(user) {
+  try {
+    const signInURL = `${serverURL}/users/signIn`;
+
+    const response = await axios.post(signInURL, user);
+    console.log('Sign in successful');
+    console.log('User data:', response.data);
+  } catch (error) {
+    console.error('Error occurred during sign in:', error.response.data);
+  }
+}
+
+// Sử dụng hàm signUp để gửi yêu cầu đăng ký
+const user = {
+  userName: 'username',
+  password: 'password',
+  email: 'example@example.com',
+  home: '123 ABC Street',
+  phone: '1234567890',
+  gender: 'female',
+  dob: new Date('1990-01-01'),
+  bankAccount: '123456789',
+  type: 'regular',
+};
+
+const user2 = {
+  userName: 'huy',
+  password: 'huy',
+}
+
+
+signIn(user2);
